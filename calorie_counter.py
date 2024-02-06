d5fe6eeebbf8abf35d41088cf003862eb695c089
Needless2Say <89311705+Needless2Say@users.noreply.github.com>
2024-02-06 12:03:17 -0500

    more food

--- calorie_counter.py
+++ calorie_counter.py
@@ -7,19 +7,19 @@ import json
 
 class CalorieCounter:
     # Data Structures
-    
+
     # All Nutrition Fact Labels and amount eaten today
     all_food_content = dict()
 
     # Units for all Nutrition Facts
     all_food_content_units = dict()
-    
+
     # food info dictionary containing each individual food dictionary that has all info related to each food
     all_food_data = dict()
-    
+
 #==================================================================================================================================
     # Functions
-    
+
     # fill all_food_content, all_food_content_amount, and all_food_content_units with nutrition facts related info
     def fill_lists(self):
         # open nutrition_facts_units json and fill following lists:
@@ -31,25 +31,25 @@ class CalorieCounter:
 
         # close file
         file.close()
-        
+
         # loop through dictionary
         for label, units in json_file.items():
             # add label to all_food_content list
             self.all_food_content[label] = 0
-            
+
             # add units to all_food_content_units
             self.all_food_content_units[label] = units
-        
+
     # fill dictionary all_food_data from food in folder to_input/food
     def fill_dicts(self):
         # open directory where all foods are stored
         os.chdir("To_Input\Foods")
-        
+
         # loop through all food files in directory
         for filename in os.listdir():
             # open file
             file = open(filename, 'r')
-            
+
             # read json file
             food_dict = json.load(file)
 
@@ -58,7 +58,7 @@ class CalorieCounter:
 
             # add food_dict to all_food_data dict
             self.all_food_data[food_dict["Name"]] = food_dict
-        
+
         # change directory back to home
         os.chdir("..\..")
 
@@ -72,7 +72,7 @@ class CalorieCounter:
 
         # close file
         file.close()
-        
+
         # loop through food_json input dict
         for food_name, food_amount in food_json.items():
             # get serving size from all_food_data dict
